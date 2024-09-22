7cc9232800ba3bc0df3b4230d5788a9525126358
AhmadFarazWeb3Developer <Ahmadfaraz.web3.0.developer@gmail.com>
2024-09-22 21:12:42 +0500

    database connected to remote cluster

--- backend/Models/dbConnection.js
+++ backend/Models/dbConnection.js
@@ -3,9 +3,13 @@ import dotenv from "dotenv";
 dotenv.config({ path: ".env" });
 import { DB_NAME } from "../Constants/database.js";
 const MONGO_URL = process.env.MONGO_URL;
+console.log(MONGO_URL);
+console.log(DB_NAME);
 
 mongoose
-  .connect(`${MONGO_URL}/${DB_NAME}`)
+  .connect(
+    `${MONGO_URL}/${DB_NAME}?retryWrites=true&w=majority&authSource=admin`
+  )
   .then(() => {
     console.log("Monogo DB Connected", mongoose.connection.host);
   })
